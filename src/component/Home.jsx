e80bfc60ef65ba2c0b495351e425a24c6c579c88
momen210 <touristmomen@gmail.com>
2024-01-03 20:54:52 +0600

    added all button, home setup

---
+++ src/component/Home.jsx
@@ -0,0 +1,14 @@
+const Home = () => {
+    return (
+        <div>
+            <h1 style={{color: 'yellow'}}>Welcome Home!</h1>
+            <h3>This is nested routes, not gonna changed</h3>
+            <button>Home</button>
+            <button>About</button>
+            <button>Blog</button>
+            <button>Contact Us</button>
+        </div>
+    );
+};
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './component/Home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
+    <App></App>
   </React.StrictMode>,
 )
