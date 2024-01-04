3448093ca3e798a8424c95ffe39428e4a98e22db
momen210 <touristmomen@gmail.com>
2024-01-04 20:24:06 +0600

    phone totally completed added to cart, details all functionality

--- src/component/Phones/PhoneDetails.jsx
+++ src/component/Phones/PhoneDetails.jsx
@@ -1,9 +1,27 @@
+import { useLoaderData } from "react-router-dom";
+
 const PhoneDetails = () => {
-    return (
-        <div>
-            <h2>Phone Details here bro</h2>
-        </div>
-    );
+  const phoneDetailsHere = useLoaderData();
+//   console.log(phoneDetailsHere.data);
+
+  const { name, image, releaseDate, mainFeatures } = phoneDetailsHere.data;
+
+  return (
+    <div>
+      <h2>Your {name} Details here</h2>
+      <img style={{ borderRadius: "7px", width: "15%" }} src={image} alt="" />
+      <p>{releaseDate}</p>
+      <p>Storage: {mainFeatures.storage}</p>
+      <div style={{ display: "flex", gap: "10px", border: "1px solid yellow", padding: "0 10px", borderRadius: "10px" }}>
+      Sensors are:
+        {mainFeatures?.sensors?.map((sens, idx) => (
+          <p key={idx}>{sens}</p>
+        ))}
+      </div>
+      <p>Chip: {mainFeatures?.chipSet}</p>
+      <p>DisplaySize: {mainFeatures.displaySize}</p>
+    </div>
+  );
 };
 
 export default PhoneDetails;
