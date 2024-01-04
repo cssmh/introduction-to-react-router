e81d6d6e46e3fd453a9580ca5248e2957b82d04f
momen210 <touristmomen@gmail.com>
2024-01-05 00:01:33 +0600

    added spinner for data loading using useNavigation

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,17 +1,18 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import "./Header.css"
 
 const Header = () => {
   return (
     <div>
       <h1 style={{ color: "yellow", marginTop: "0px" }}>React Router</h1>
       <nav>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/about"}>About</Link>
-        <Link to={"/users"}>Users</Link>
-        <Link to={"/posts"}>Posts</Link>
-        <Link to={"/phones"}>Phones</Link>
-        <Link to={"/blog"}>Blog</Link>
-        <Link to={"/contact"}>Contact Us</Link>
+        <NavLink to={""}>Home</NavLink>
+        <NavLink to={"/about"}>About</NavLink>
+        <NavLink to={"/users"}>Users</NavLink>
+        <NavLink to={"/posts"}>Posts</NavLink>
+        <NavLink to={"/phones"}>Phones</NavLink>
+        <NavLink to={"/blog"}>Blog</NavLink>
+        <NavLink to={"/contact"}>Contact Us</NavLink>
       </nav>
     </div>
   );
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,13 +1,28 @@
-import { Outlet } from "react-router-dom";
-import './Home.css'
+import { Outlet, useNavigation } from "react-router-dom";
+import "./Home.css";
 import Header from "./Header";
 import Footer from "./Footer";
+import { ColorRing } from "react-loader-spinner";
 
 const Home = () => {
+  const navigation = useNavigation();
+
   return (
     <div>
       <Header></Header>
-      <Outlet></Outlet>
+      {navigation.state === "loading" ? (
+        <ColorRing
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="color-ring-loading"
+          wrapperStyle={{}}
+          wrapperClass="color-ring-wrapper"
+          colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+        />
+      ) : (
+        <Outlet></Outlet>
+      )}
       <Footer></Footer>
     </div>
   );
